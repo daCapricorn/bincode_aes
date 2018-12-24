54d884975bd7696a1a4ce7799a09a78bfca17da6
James Howard <jrobhoward@gmail.com>
2018-12-23 21:28:17 -0600

    requires doc update, todo, and tests. also explain inefficiencies:   * overhead for each struct that gets serialized   * copy that happens during decrypt   * why deserialize accepts a mutable reference

--- examples/testcrypt.rs
+++ examples/testcrypt.rs
@@ -1,6 +1,5 @@
 #[macro_use]
 extern crate serde_derive;
-extern crate rand;
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 struct Entity {
@@ -12,11 +11,8 @@ struct Entity {
 struct World(Vec<Entity>);
 
 fn main() {
-    //let iv = bincode_aes::random_iv();
-    //let mut bc = bincode_aes::new();
     let key = bincode_aes::random_key();
-    let iv = bincode_aes::random_iv();
-    let mut bc = bincode_aes::with_params(key, iv);
+    let bc = bincode_aes::with_key(key);
 
     let world = World(vec![Entity { x: 0.0, y: 4.0 }, Entity { x: 10.0, y: 20.5 }]);
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,75 +1,101 @@
-#![cfg_attr(feature = "cargo-clippy", allow(clippy::cast_ptr_alignment))]
+//! # bincode_aes
+//! `bincode_aes` wraps bincode.  It encrypts data as it is encoded, and decrypts the data as it is decoded.
+//!
+//! ### Using Basic Functions
+//!
+//! ```rust
+//! extern crate bincode_aes;
+//! fn main() {
+//!     // The object that we will serialize.
+//!     let key = bincode_aes::random_key();
+//!     let bc = bincode_aes::with_key(key);
+//!     let target: Option<String>  = Some("hello world".to_string());
+//!
+//!     let mut encoded: Vec<u8>    = bc.serialize(&target).unwrap();
+//!     let decoded: Option<String> = bc.deserialize(&mut encoded).unwrap();
+//!     assert_eq!(target, decoded);
+//! }
+//! ```
 
+#[macro_use]
+extern crate serde_derive;
 extern crate bincode;
 extern crate crypto;
 extern crate rand;
 
-pub use bincode::{Error, ErrorKind};
 use crypto::buffer::{BufferResult, ReadBuffer, WriteBuffer};
 use crypto::{aes, blockmodes, buffer, symmetriccipher};
 use rand::rngs::OsRng;
 use rand::Rng;
 
-// constant sizes used for crypto operations
+/// size of in-mem buffers used for crypto operations
 const BUFFER_SIZE: usize = 8192;
+/// key length (AES-256-CBC)
 const KEY_LEN: usize = 32;
+/// initialization vector length (AES-256-CBC)
 const IV_LEN: usize = 16;
 
-// wrapped types
-pub struct Key(Vec<u8>);
+// distinct/strong types wrapping u8 vectors
+#[derive(Serialize, Deserialize)]
+pub struct EncryptedData(Vec<u8>);
+#[derive(Serialize, Deserialize)]
 pub struct IV(Vec<u8>);
+pub struct Key(Vec<u8>);
 
-pub struct EncryptedSerializer {
-    key: Key,
+#[derive(Serialize, Deserialize)]
+struct SerializedResult {
     iv: IV,
+    encrypted_data: EncryptedData,
 }
 
-pub fn new() -> EncryptedSerializer {
-    EncryptedSerializer {
-        key: random_key(),
-        iv: random_iv(),
-    }
+pub struct BincodeCryptor {
+    key: Key,
 }
 
-pub fn with_params(key: Key, iv: IV) -> EncryptedSerializer {
-    EncryptedSerializer {
-        key,
-        iv,
-    }
+/// Returns a keyed BincodeCryptor
+pub fn with_key(key: Key) -> BincodeCryptor {
+    BincodeCryptor { key }
 }
 
-impl EncryptedSerializer {
-    pub fn serialize<T: ?Sized>(&mut self, value: &T) -> bincode::Result<Vec<u8>>
+impl BincodeCryptor {
+    /// Serializes a serializable object into a `Vec` of bytes
+    pub fn serialize<T: ?Sized>(&self, value: &T) -> bincode::Result<Vec<u8>>
     where
         T: serde::Serialize,
     {
-        let decrypted = bincode::serialize(value)?;
-        //todo: replace with to_slice
-        let encrypted = encrypt(&decrypted[..], &self.key, &self.iv).unwrap();
+        let iv = random_iv();
+        let bincoded_value = bincode::serialize(value)?;
+        let encrypted_value = encrypt(bincoded_value.as_slice(), &self.key, &iv).unwrap();
+        let encrypted_data = EncryptedData(encrypted_value);
 
-        Ok(encrypted.clone())
+        let serialized_result = SerializedResult { iv, encrypted_data };
+        let result = bincode::serialize(&serialized_result).unwrap();
+
+        Ok(result)
     }
 
-    pub fn deserialize<'b, T>(&'b mut self, bytes: &'b mut Vec<u8>) -> bincode::Result<T>
+    /// Deserializes a slice of bytes into an instance of `T`
+    pub fn deserialize<'a, T>(&'a self, bytes: &'a mut Vec<u8>) -> bincode::Result<T>
     where
-        T: serde::de::Deserialize<'b>,
+        T: serde::de::Deserialize<'a>,
     {
-        let decrypted = decrypt(bytes.as_slice(), &self.key, &self.iv).ok().unwrap();
+        let serialized_result: SerializedResult = bincode::deserialize(&bytes[..]).unwrap();
+
+        // ideally, we would decrypt in-place
+        let decrypted_data = decrypt(
+            &serialized_result.encrypted_data.0.as_slice(),
+            &self.key,
+            &serialized_result.iv,
+        )
+        .ok()
+        .unwrap();
         bytes.clear();
-        bytes.extend_from_slice(decrypted.as_slice());
+        bytes.extend_from_slice(decrypted_data.as_slice());
         bincode::deserialize(&bytes[..])
-    }
-
-    pub fn update_key(&mut self) {
-        self.key = random_key();
-    }
-
-    pub fn update_iv(&mut self) {
-        self.iv = random_iv();
     }
 }
 
-/// Creates a random AES key.
+/// creates a random AES key
 pub fn random_key() -> Key {
     let mut key = vec![0; KEY_LEN];
     let mut rng = OsRng::new().unwrap();
@@ -77,15 +103,14 @@ pub fn random_key() -> Key {
     Key(key)
 }
 
-/// Creates a random AES initialization vector.
-pub fn random_iv() -> IV {
+fn random_iv() -> IV {
     let mut iv = vec![0; IV_LEN];
     let mut rng = OsRng::new().unwrap();
     rng.fill(&mut iv[..]);
     IV(iv)
 }
 
-pub fn encrypt(
+fn encrypt(
     data: &[u8],
     key: &Key,
     iv: &IV,
@@ -124,7 +149,7 @@ pub fn encrypt(
     Ok(final_result)
 }
 
-pub fn decrypt(
+fn decrypt(
     encrypted_data: &[u8],
     key: &Key,
     iv: &IV,
@@ -160,5 +185,3 @@ pub fn decrypt(
 
     Ok(final_result)
 }
-
-
